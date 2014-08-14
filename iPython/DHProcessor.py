9459c611ec80ced87094e915a2c46ee924addd3c
Jakub Kakona <kaklik@mlab.cz>
2014-08-15 00:09:26 +0200

    improved notebook layout.

--- iPython/DHProcessor.py
+++ iPython/DHProcessor.py
@@ -125,26 +125,21 @@ def get_values(url_meteors, x):
     [ahtml4, ahtml7, anurl, aaanurl]=list_returned
     ahtml4=ahtml4[:x]
     
-    data=[]
+    data = pd.DataFrame([])
     for filename in ahtml4:   
         if filename in ahtml7:
-            data.append(pd.read_csv(aaanurl + filename[1:], names = ["a", "b", "c", "d", "e"], sep = ";"))
+            data = pd.DataFrame.append(data, pd.read_csv(aaanurl + filename[1:], index_col='# file name', sep = ";"))
         else:
-            data.append(pd.read_csv(anurl + filename[1:], names = ["a", "b", "c", "d", "e"], sep = ";"))  
-            
-    freq_data = []
+            data = pd.DataFrame.append(data, pd.read_csv(anurl + filename[1:], index_col='# file name', sep = ";"))  
 
-    for f in range(len(data)):   
-        x=[int(round(i)) for i in data[f]["c"]]
-        freq_data=freq_data+x
-    return [freq_data, ahtml4]
+    return data
 
 def process_histogram(url_meteors, x, obsname):
     get_values(url_meteors, x)
-    thelist=get_values(url_meteors, x)
-    [freq_data, ahtml4]=thelist
+    data=get_values(url_meteors, x)
     plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
-    plt.hist(thelist[0], bins=100)
+    pd.DataFrame.hist(data, column = 1, bins = 100,)
+
     plt.title("Recent observations histogram"+" ("+obsname+")")
     plt.xlabel("Frequency (Hz)")
     plt.ylabel("Count")
@@ -161,9 +156,4 @@ def process_table(url_meteors, x, obsname):
         print aaa[7:9]+'/'+ aaa[5:7]+'/'+ aaa[1:5], aaa[9:11]+':'+ aaa[11:13]
     print '......'
 
-
-process_histogram("http://space.astro.cz/bolidozor/OBSUPICE/OBSUPICE-R1/data/", 6, "Upice Observatory")
-process_histogram("http://space.astro.cz/bolidozor/uFlu/uFlu-R0/data/", 6, "FLuOBS")
-process_table("http://space.astro.cz/bolidozor/OBSUPICE/OBSUPICE-R1/data/", 6, "Upice Observatory")
-process_table("http://space.astro.cz/bolidozor/uFlu/uFlu-R0/data/", 6, "FLuOBS")
 
