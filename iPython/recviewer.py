0d851af29317333aa62850befc9f085ff291ba0c
Jakub Kákona <kaklik@mlab.cz>
2015-03-17 12:51:08 +0100

    Bolidozor raws FITS data viewer.

--- iPython/recviewer.py
+++ iPython/recviewer.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python
-
 # Experimental record viewer, recalculates the waterfall
-# with different number of bins according to its visual stretching.
 
 import numpy as np
 import Queue as queue
@@ -10,20 +7,6 @@ import scipy.io.wavfile
 import sys
 import os.path
 import math
-
-from OpenGL.GL import *
-from OpenGL.GLUT import *
-
-#from pysdr.waterfall import *
-#from pysdr.overlay import *
-#import pysdr.ext as ext
-
-import sys
-from vispy import color
-from vispy import scene
-from vispy import app, plot
-
-
 
 def waterfallize(signal, bins):
     window = 0.5 * (1.0 - np.cos((2 * math.pi * np.arange(bins)) / bins))
@@ -68,40 +51,6 @@ def waterfallize(signal, bins):
 
         self.worker.set_work(texture_work, (self, bins))
 '''
-def view(signal, sample_rate=None):
-#    canvas = scene.SceneCanvas(keys='interactive')
-#    canvas.size = 800, 600
-#    canvas.show()
-#    record_viewer = RecordViewer(signal, sample_rate=sample_rate)
-
-    # Set up a viewbox to display the image with interactive pan/zoom
-#    view = canvas.central_widget.add_view()
-
-    import matplotlib.pyplot as plt
-
-    cmap = plt.get_cmap('jet')
-
-
-
-    # Create the image
-    img_data = np.random.normal(size=(100, 100, 2), loc=128,
-                                scale=50).astype(np.ubyte)
-    bins = 4096
-#    cm = color.get_colormap('ice')
-    waterfall = waterfallize(signal, bins)
-    waterfall[np.isneginf(waterfall)] = np.nan
-    wmin, wmax = np.nanmin(waterfall), np.nanmax(waterfall)
-    waterfall = ((waterfall - wmin) / (wmax - wmin)) * 5.5 - 4.5
-#    waterfall = cm.map(waterfall)
-
-    rgba_img = cmap(waterfall)
-    rgb_img = np.delete(rgba_img, 3, 2)
-    print rgb_img*255
-
-#    image = scene.visuals.Image(waterfall, parent=view.scene)
-    image = plot.image(waterfall.astype(np.ubyte))
-    # Set the view bounds to show the entire image with some padding
-#    view.camera.rect = (-10, -10, image.size[0]+20, image.size[1]+20)
 
 def read_file(filename):
     ext = os.path.splitext(filename)[1]
@@ -124,18 +73,8 @@ def read_file(filename):
             raise Exception("width %d is not a multiple of 2" % (size[0],))
 
         flat_data = np.ravel(img.data)
-        return (44800, flat_data[0::2] + 1j * flat_data[1::2])
+        return (48000, flat_data[0::2] + 1j * flat_data[1::2])
     else:
         raise Exception("unknown filename extension: %s" % (ext,))
 
-def main():
-    if len(sys.argv) != 2:
-        sys.stderr.write("usage: recordviewer.py FILENAME\n")
-        exit(1)
 
-    sample_rate, signal = read_file(sys.argv[1])
-    view(signal, sample_rate=sample_rate)
-
-if __name__ == "__main__":
-    main()
-    app.run()
